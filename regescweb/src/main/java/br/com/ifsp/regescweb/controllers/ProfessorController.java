76fcd677671a54a81e4a5d716c2b2df83a834639
Giovana Soares <giihs2005@gmail.com>
2022-05-15 20:05:46 -0300

    Evitando o Web Parameter Tampering

--- regescweb/src/main/java/br/com/ifsp/regescweb/controllers/ProfessorController.java
+++ regescweb/src/main/java/br/com/ifsp/regescweb/controllers/ProfessorController.java
@@ -1,11 +1,13 @@
 package br.com.ifsp.regescweb.controllers;
 
+import br.com.ifsp.regescweb.dto.RequisicaoNovoProfessor;
 import br.com.ifsp.regescweb.models.Professor;
 import br.com.ifsp.regescweb.models.StatusProfessor;
 import br.com.ifsp.regescweb.repositories.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 
@@ -29,5 +31,13 @@ public class ProfessorController {
         ModelAndView mv = new ModelAndView( "professores/new");
         mv.addObject("statusProfessor", StatusProfessor.values());
         return mv;
+    }
+
+    @PostMapping("/professores")
+    public String create(RequisicaoNovoProfessor requisicao){
+        Professor professor = requisicao.toProfessor();
+        this.professorRepository.save(professor);
+
+        return "redirect:/professores";
     }
 }
---
+++ regescweb/src/main/java/br/com/ifsp/regescweb/dto/RequisicaoNovoProfessor.java
@@ -0,0 +1,55 @@
+package br.com.ifsp.regescweb.dto;
+
+import java.math.BigDecimal;
+
+import br.com.ifsp.regescweb.models.Professor;
+import br.com.ifsp.regescweb.models.StatusProfessor;
+
+public class RequisicaoNovoProfessor {
+    private String nome; 
+    private BigDecimal salario;
+    private StatusProfessor statusProfessor;
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public StatusProfessor getStatusProfessor() {
+        return statusProfessor;
+    }
+
+    public void setStatusProfessor(StatusProfessor statusProfessor) {
+        this.statusProfessor = statusProfessor;
+    }
+
+    public Professor toProfessor(){
+        Professor professor = new Professor();
+        professor.setNome(this.nome);
+        professor.setSalario(this.salario);
+        professor.setStatusProfessor(this.statusProfessor);
+    
+        return professor; 
+    }
+
+    @Override
+    public String toString() {
+        return "RequisicaoNovoProfessor{" +
+                "nome='" + nome + '\'' +
+                ", statusProfessor=" + statusProfessor +
+                ", salario=" + salario +
+                '}';
+    }
+    
+}
