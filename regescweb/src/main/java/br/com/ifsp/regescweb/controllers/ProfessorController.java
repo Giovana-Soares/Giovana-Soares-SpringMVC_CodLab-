7d98f47ed8775371b516d41c9ba7d4cd9fe621c0
Giovana Soares <giihs2005@gmail.com>
2022-05-15 16:28:30 -0300

    Buscando registros do banco com Spring Data

--- regescweb/src/main/java/br/com/ifsp/regescweb/controllers/ProfessorController.java
+++ regescweb/src/main/java/br/com/ifsp/regescweb/controllers/ProfessorController.java
@@ -1,32 +1,25 @@
 package br.com.ifsp.regescweb.controllers;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.List;
-
+import br.com.ifsp.regescweb.models.Professor;
+import br.com.ifsp.regescweb.repositories.ProfessorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import br.com.ifsp.regescweb.models.Professor;
-import br.com.ifsp.regescweb.models.StatusProfessor;
-
+import java.util.List;
 
 @Controller
-public class ProfessorController{
- 
+public class ProfessorController {
+    @Autowired
+    private ProfessorRepository professorRepository;
+
     @GetMapping("/professores")
     public ModelAndView index(){
-        Professor batman= new Professor ("Batman", new BigDecimal (5000.0), StatusProfessor.ATIVO);
-        batman.setId(1L);
-        Professor coringa = new Professor ("Coringa", new BigDecimal(10000.0), StatusProfessor.APOSENTADO);
-        coringa.setId(2L);
-        Professor mulherMaravilha = new Professor("Mulher Maravilha", new BigDecimal(15000.0), StatusProfessor.INATIVO);
-        mulherMaravilha.setId(3L);
 
-        List<Professor> professores = Arrays.asList(batman, coringa, mulherMaravilha);
+        List<Professor> professores = this.professorRepository.findAll();
+
         ModelAndView mv = new ModelAndView("professores/index");
-        mv.addObject( "professores", professores);
+        mv.addObject("professores", professores);
         return mv;
     }
 }
---
+++ regescweb/src/main/java/br/com/ifsp/regescweb/repositories/ProfessorRepository.java
@@ -0,0 +1,10 @@
+package br.com.ifsp.regescweb.repositories;
+
+import br.com.ifsp.regescweb.models.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Long>{
+
+}
